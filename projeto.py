92186e0ba494a585020b4345bf14122eaf540896
jotaaapeee <joao.varrichio@hotmail.com>
2025-09-08 14:33:21 -0300

    graphData template

--- projeto.py
+++ projeto.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 def loadData(CSVName):
@@ -25,9 +25,18 @@ def prepareData(data):
 
     print(data)
 
+def graphData(data):
+    plt.figure(figsize=(10, 6))
+    plt.hist(data['animalage'], bins=20, color='skyblue', edgecolor='black')
+    plt.title('Distribuição de Idade dos Animais')
+    plt.xlabel('Idade')
+    plt.ylabel('Frequência')
+    plt.show()
+
 CSVName = 'animal-data-1.csv'
 print(CSVName)
 data = loadData(CSVName)
 print(data)
 data = prepareData(data)
 print(data)
+graphData(data)
